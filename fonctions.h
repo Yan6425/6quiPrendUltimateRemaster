dc29be801728480e80c601aae1d0f99172322a9c
gabin <alquiergab@cy-tech.fr>
2023-05-23 17:14:41 +0200

    signature des structures

--- fonctions.h
+++ fonctions.h
@@ -1,3 +1,9 @@
 #ifndef __fonctions_H_
 #define __fonctions_H_
+
+typedef struct Joueur Joueur; // sert à modéliser un joueur ainsi que son nombre de points
+typedef struct Carte Carte; // sert à modéliser une carte
+typedef struct Noeud Noeud; // sert a modéliser un noeud d'une liste chainée 
+
+
 #endif
