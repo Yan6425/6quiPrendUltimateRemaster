3be8c357c681957efb4b8b61897f83c1de01f6d8
yan.hourdebaigt.hiroux@gmail.com <hourdebaig@pce10820.etude.pau.eisti.fr>
2023-05-25 15:36:22 +0200

    Revert "création branche avance"
    This reverts commit 57477a2897f792a7e643fefadb575ebc22c0a8bb.

--- fonctions.c
+++ fonctions.c
@@ -3,90 +3,36 @@
 #include <stdlib.h>
 
 
-typedef struct Joueur{
-    char* nom;
-    Carte* main;
-} Joueur;
-
-
-typedef struct Carte{
-    int numero;
-    int valeur;
-} Carte;
-
-
-typedef struct Noeud{
-    Carte* carte;
-    Noeud* suivant;
-} Noeud;
-
-
-void creerJoueurs(int nombreJoueurs){
-    // explication de la fonction
-
-}
-
-
-Joueur _creerJoueur(char* nom){
-    Joueur joueur;
-    joueur.nom = nom;
-    joueur.main = malloc(10 * sizeof(Carte));
-    return joueur;
-}
-
-
-void reglages(){
-    // explication de la fonction
-
-}
-
-
-void lancerPartie(){
+void _6QuiPrend(){
     // explication de la fonction
 }
 
 
-int _distribution(){
+int lancerPartie(){
     // explication de la fonction
     return 0;
 }
 
 
-Carte _creerCarte(int numero){
-    Carte carte;
-    carte.numero = numero;
-    if (numero % 10 == 5){
-        carte.valeur = 2;
-    }
-    else if (numero % 10 == 0){
-        carte.valeur = 3;
-    }
-    else if (numero % 11 == 0){
-        carte.valeur = 5;
-    }
-    else if (numero == 5){
-        carte.valeur = 7;
-    }
-    else {
-        carte.valeur = 1;
-    }
-    return carte;
-}
-
-
-int _choixCarte(){
+int distribution(){
     // explication de la fonction
     return 0;
 }
 
 
-int _comparaisons(){
+int choixCarte(){
     // explication de la fonction
     return 0;
 }
 
 
-int _distributionPts(){
+int comparaisons(){
+    // explication de la fonction
+    return 0;
+}
+
+
+int distributionPts(){
     // explication de la fonction
     return 0;
 }
--- fonctions.h
+++ fonctions.h
@@ -5,14 +5,11 @@ typedef struct Joueur Joueur; // sert à modéliser un joueur ainsi que son nomb
 typedef struct Carte Carte; // sert à modéliser une carte
 typedef struct Noeud Noeud; // sert a modéliser un noeud d'une liste chainée 
 
-void creerJoueurs(int nombreJoueurs);
-Joueur _creerJoueur(char* nom);
-void reglages();
-void lancerPartie();
-int _distribution();
-Carte _creerCarte(int numero);
-int _choixCarte();
-int _comparaisons();
-int _distributionPts();
+void _6QuiPrend();
+int lancerPartie();
+int distribution();
+int choixCarte();
+int comparaisons();
+int distributionPts();
 
 #endif
