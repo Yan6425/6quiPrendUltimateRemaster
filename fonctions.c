56d3f17843c02c8ee5266e3600e21b2a842e6f3b
gabin <alquiergab@cy-tech.fr>
2023-05-25 14:59:03 +0200

    premieres struct

--- fonctions.c
+++ fonctions.c
@@ -1,3 +1,13 @@
 #include "fonctions.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct Joueur{
+    char* nom;
+    Carte* main;
+} Joueur;
+
+typedef struct Carte{
+    int numero;
+    int valeur;
+} Carte;
