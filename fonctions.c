93ba6fde7ce22602144e610904d85043bf231196
Rayan <rayan.ayoub31180@gmail.com>
2023-05-25 15:15:07 +0200

    commentaires

--- fonctions.c
+++ fonctions.c
@@ -7,6 +7,6 @@ typedef struct Joueur{
 } Joueur;
 
 typedef struct Carte{
-    int numero;
-    int valeur;
+    int numero;     //numéro carte de 1 à 104
+    int valeur;     //nombre tête de boeuf de 1 à 7
 } Carte;
