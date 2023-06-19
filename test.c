792e1c811fc94c72bcac53149824f284f35de9be
Yan <yan.hourdebaigt.hiroux@gmail.com>
2023-06-19 14:57:13 +0200

    tmp

--- test.c
+++ test.c
@@ -3,13 +3,31 @@
 #include <stdlib.h>
 #include <assert.h>
 
+typedef struct Carte{
+    int numero;     //numéro carte de 1 à 104
+    int valeur;     //nombre tête de boeuf de 1 à 7
+} Carte;
+
+
+typedef struct Joueur{
+    char* nom;
+    Carte* main;
+    int nbPoints;
+} Joueur;
+
+
+typedef struct Noeud{
+    Carte carte;
+    Noeud* suivant;
+} Noeud;
+
+
 int main(){
-    /*
     Joueur joueur = creerJoueur("Bob");
     assert(joueur.nom = "Bob");
     assert(joueur.nbPoints = 0);
     assert(joueur.main = Carte*);
     assert(sizeof(joueur.main) = 10 * sizeof(Carte));
-    */
+    
     return 0;
 }
