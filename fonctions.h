0e12ca6c7b35b46897ba3d981d00f56675851fd6
Rayanyb <rayan.ayoub31180@gmail.com>
2023-06-19 20:26:31 +0200

    commentaire et ajout de fonction dans le .h

--- fonctions.h
+++ fonctions.h
@@ -2,21 +2,22 @@
 #define __fonctions_H_
 
 typedef struct Joueur Joueur; // sert à modéliser un joueur ainsi que son nombre de points et sa main
-Joueur creerJoueur(char* nom);
+Joueur creerJoueur(char* nom);// cree un joueur avec un nom, une main et un nombre de carte
 typedef struct Carte Carte; // sert à modéliser une carte
-Carte creerCarte(int numero);
+Carte creerCarte(int numero);//cree une carte avec son numéro et sa valeur (nb de taureau)
 typedef struct Noeud Noeud; // sert a modéliser un noeud d'une liste chainée 
 void insererNoeud(Noeud** liste, Carte carte, int index);
 Noeud* extraireNoeud(Noeud** liste, int index);
 
-void sixQuiPrend();
+void sixQuiPrend();//ratio
 void reglages();// sert à parametrer le jeu
-void lancerPartie(Joueur* tblJoueurs, int nbJoueurs, int nbCartes);
+void lancerPartie(Joueur* tblJoueurs, int nbJoueurs, int nbCartes);//mets en place le plateau et la distribution
 Joueur* creerTblJoueurs(int nbJoueurs);//rassemble tous les joueurs dans un tableau
 int distribution(Noeud** plateau, Joueur* tblJoueurs, int nbJoueurs, int nbCartes);
+void ajouterNoeud(Noeud** liste, Carte carte);
 Noeud* melangerCartes(int nbCartes);
-Carte* distribuerMain();
-int choixCarte(Joueur joueur, int nbCartes);
+Carte* distribuerMain(Noeud** paquet);//distribue les mains aux joueurs
+int choixCarte(Joueur joueur, int nbCartes);//le joueur choisi parmis ses 10 cartes en main
 int triCarte();
 int comparaisons();
 int distributionPts(Noeud** plateau, Joueur* tblJoueurs, int nbJoueurs, int nbCartes);
