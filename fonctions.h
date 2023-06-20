a548d6579342c9df6d590607cb882353b85cd3dd
Rayanyb <rayan.ayoub31180@gmail.com>
2023-06-20 21:54:48 +0200

    commentaires + ajustement .h

--- fonctions.h
+++ fonctions.h
@@ -8,7 +8,7 @@ Joueur creerJoueur(char* nom);// cree un joueur avec un nom, une main et un nomb
 Joueur* creerTblJoueurs(int nbJoueurs);//rassemble tous les joueurs dans un tableau
 typedef struct Noeud Noeud; // sert a modéliser un noeud d'une liste chainée 
 void insererNoeud(Noeud** liste, Carte carte, int index);//création et définition de la taille des noeuds 
-void triInsertion(Noeud** liste, Carte carte);
+void triInsertion(Noeud** liste, Carte carte);//Trier la liste chainée de carte par insertion
 Carte extraireNoeud(Noeud** liste, int index);// permet d'extraire un noeud d'une liste chainée
 void affListe(Noeud* liste);//afficher le numéro la valeur et la taille de la liste
 
@@ -18,7 +18,6 @@ void lancerPartie(Joueur* tblJoueurs, int nbJoueurs, int nbCartes);//mets en pla
 void distribution(Noeud** plateau, Joueur* tblJoueurs, int nbJoueurs, int nbCartes);//Distribuer les cartes aux joueurs
 Noeud* melangerCartes(int nbCartes);//mélanger les cartes dans le paquets avec un tri aléatoire
 Carte* creerMain(Noeud** paquet);//Extraire 10 cartes du paquets pour créer la main du joueur
-void triInsertion(Noeud** liste, Noeud* nouvNoeud);
 Carte choixCarte(Joueur* joueur, int nbCartes);//le joueur choisi parmis ses 10 cartes en main
 void nettoyerPlateau(Noeud** plateau);
 void placerCarte(Noeud** plateau, Carte carte);
